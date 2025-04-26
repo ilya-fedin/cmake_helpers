4f58f38d0035ba9e453dd398fd4a3d4f8600645a
Ilya Fedin <fedin-ilja2010@ya.ru>
2025-04-27 01:42:05 +0400

    Automatically use vcpkg

--- validate_special_target.cmake
+++ validate_special_target.cmake
@@ -21,9 +21,38 @@ function(report_bad_special_target)
     endif()
 endfunction()
 
-if (APPLE AND NOT DESKTOP_APP_USE_PACKAGED)
-    set(CMAKE_OSX_DEPLOYMENT_TARGET 10.13 CACHE STRING "Minimum macOS deployment version")
-    set(CMAKE_OSX_ARCHITECTURES "x86_64;arm64" CACHE STRING "Target macOS architectures")
+set(VCPKG 0)
+if (DESKTOP_APP_USE_PACKAGED)
+    if (DEFINED ENV{VCPKG_ROOT})
+        set(CMAKE_TOOLCHAIN_FILE $ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake CACHE FILEPATH "The CMake toolchain file")
+    endif()
+
+    if (CMAKE_TOOLCHAIN_FILE MATCHES "/vcpkg.cmake$")
+        set(VCPKG 1)
+    endif()
+endif()
+
+if (VCPKG)
+    if (DEFINED ENV{VSCMD_ARG_HOST_ARCH})
+        set(VCPKG_HOST_TRIPLET "$ENV{VSCMD_ARG_HOST_ARCH}-windows" CACHE STRING "Vcpkg host triplet (ex. x86-windows)")
+    endif()
+
+    if (DEFINED ENV{VSCMD_ARG_TGT_ARCH})
+        set(VCPKG_TARGET_TRIPLET "$ENV{VSCMD_ARG_TGT_ARCH}-windows" CACHE STRING "Vcpkg target triplet (ex. x86-windows)")
+    endif()
+
+    set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Choose the type of build")
+    set(PKG_CONFIG_ARGN "--static" CACHE STRING "Arguments to supply to pkg-config")
+endif()
+
+if (APPLE)
+    if (NOT DESKTOP_APP_USE_PACKAGED OR VCPKG)
+        set(CMAKE_OSX_DEPLOYMENT_TARGET 10.13 CACHE STRING "Minimum macOS deployment version")
+    endif()
+
+    if (NOT DESKTOP_APP_USE_PACKAGED)
+        set(CMAKE_OSX_ARCHITECTURES "x86_64;arm64" CACHE STRING "Target macOS architectures")
+    endif()
 endif()
 
 if (WIN32)
